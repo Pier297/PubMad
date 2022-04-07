3c5f85b872d03e89bee5c6a796d5fdf0024f01e1
William Simoni <wilsimoni@gmail.com>
2022-04-07 12:16:52 +0200

    Updated Entity class

--- src/data/Entity.py
+++ src/data/Entity.py
@@ -1,7 +1,13 @@
+from itertools import tee
+from tokenize import Number
 from typing import Optional
 from dataclasses import dataclass
 
+from matplotlib.pyplot import text
+
 @dataclass
 class Entity:
-    text: str
-    
+    id: str
+    mention: str
+    obj: str
+    prob: float
